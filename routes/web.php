a21463a1a3b5e737003e416f970f678c72315c56
rirakib <rirrakib10@gmail.com>
2022-02-10 20:31:47 +0600

    login page design

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/',[DashboardController::class,'index'])->name('dashboard.index');
+
+Route::get('/login',[LoginController::class,'login'])->name('login');
